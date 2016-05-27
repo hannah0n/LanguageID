96c4c889be003d2161bf768e0acfcb911447acd0
Josue Rios <jrios777@gitlab.cs.washington.edu>
2016-05-27 14:21:00 -0700

    Added some files, modified new

---
+++ identifier.py
@@ -0,0 +1,19 @@
+# This is the program that learns using N-grams and identifies the language of
+# each of the development set
+
+import sys
+
+# Strips the given file and uses the tokenizer
+# def preprocess():
+#     pass
+
+
+def main(args):
+    # Train & Develop Model
+    # Run Model on Development set
+    # Calculate the Precision and Recall
+    # Optionally run on Test set
+    pass
+
+if __name__ == "__main__":
+    main(sys.argv)
